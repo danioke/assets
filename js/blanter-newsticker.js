7d88eb3d46ee83062529cefc2f0dc01facf26e0d
Mardani <36178410+danioke@users.noreply.github.com>
2024-05-25 00:14:23 +0700

    Update blanter-newsticker.js

--- js/blanter-newsticker.js
+++ js/blanter-newsticker.js
@@ -52,13 +52,12 @@ function recent_post_style(){
     s += "#recent_post{";
     s += "margin:0px;";
     s += "width:auto;";
-    s += "background:#fff;";
     s += "}";
     s += "</style>";
     document.write(s);
 }
 function recent_post_content(){
-    var s = "<div id='recent_post' title='<div class='jam' id='jam'>'>";
+    var s = "<div id='recent_post' title='<div class='jam' id='jam'>'";
     if(info_text){
     s += "<div class='wrapper'>";
     s += "<div class='newstitle'>";

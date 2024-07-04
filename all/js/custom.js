f0026eb59b2d5c1eeeec8b6b837d1f7ab3aaa879
Mardani <36178410+danioke@users.noreply.github.com>
2024-07-04 11:35:39 +0700

    Update custom.js
    author to post.author

--- all/js/custom.js
+++ all/js/custom.js
@@ -1341,7 +1341,7 @@ $(document).ready(function(){
         function activate_pageGreeting(){
             var greetingTime = new Date().getHours();
             var greetingMessage;
-            var greetingUser = $('.greeting-text').data('author')
+            var greetingUser = $('.greeting-text').data('post.author')
             var greetingMorning = ('Good morning');
             var greetingAfternoon = ('Good afternoon');
             var greetingEvening = ('Good evening');
